2d4f1f4b0c7ff73237129f5e791e64632a75b9ea
Frontend JavaScript <frontendjavascript@C3-03.local>
2023-02-21 20:48:29 +0200

    c

--- curs6/js/ap2.js
+++
@@ -1,5 +0,0 @@
-let adunare = (a, b) => {
-    return a + b;
-}
-
-console.log ("Suma dintre 24 si 46 este", adunare(24, 46));
--- curs6/js/main.js
+++
@@ -1,9 +0,0 @@
-import {citeste} from './book.js'
-import Book from './book.js'
-
-//import * from "demo.js"
-//import {func1, obj2} from "demo.js"
-
-let carte = new Book('Poezii', 'Mihai Eminescu', 250);
-console.log(carte.toString());
-console.log(citeste());
